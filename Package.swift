0e8180dcceec724974d4d0a0076a4af6930b37ac
Jhonatan Pulgarin Arias <jpulgarin@eogresources.com>
2022-08-10 14:09:58 -0500

    no message

--- Package.swift
+++ Package.swift
@@ -5,20 +5,18 @@ import PackageDescription
 
 let package = Package(
     name: "ConnectivityFramework",
-    platforms: [.iOS(.v15)],
+    platforms: [
+        .iOS(.v15)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ConnectivityFramework",
             targets: ["ConnectivityFramework"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
 //        .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk.git", from: "8.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ConnectivityFramework",
             dependencies: [
