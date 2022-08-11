593d2e9a807b4a1a759bfaaacb30555263c27831
Maury Marmolejo <maury.mdin@gmail.com>
2022-08-11 10:01:38 -0500

    LoginToEmail

--- Sources/Services/LoginService.swift
+++ Sources/Services/LoginService.swift
@@ -1,22 +1,56 @@
 import Foundation
+import FirebaseAuth
+
+//MARK: - Enum for Login Type
+enum ProviderType: String {
+    case basic
+    case google
+    case facebook
+}
 
 // MARK: - Protocol
 public protocol LoginServiceProtocol {
     
-    func login(email: String, password: String)
+    func login(email: String, password: String, completionError: @escaping ((Error?) -> Void))
 }
 
 public class LoginService {
     
-    public init() {
-        print("From library")
+    //MARK: - Variables
+    
+    private(set) var email: String
+    private(set) var provider: ProviderType
+    
+    init(email: String, provider: ProviderType) {
+        self.email = email
+        self.provider = provider
     }
+    
 }
 
 // MARK: - LoginServiceProtocol
 extension LoginService: LoginServiceProtocol {
     
-    public func login(email: String, password: String) {
-        print("Login.....")
+    public func saveDataLogin() {
+        
+        let defaults = UserDefaults.standard
+        defaults.set(email, forKey: "email")
+        defaults.set(provider.rawValue, forKey: "provider")
+        defaults.synchronize()
+    }
+    
+    public func login(email: String, password: String, completionError: @escaping ((Error?) -> Void)) {
+        
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            if error == nil {
+                completionError(error)
+            } else {
+                Auth.auth().createUser(withEmail: email, password: password) { result, error in
+                    
+                    completionError(error)
+                }
+            }
+                
+        }
     }
 }
